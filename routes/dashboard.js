074734441396d1fd06f880448a5cc8bb8724c2b8
George Calvo <george.r.calvo@gmail.com>
2018-02-11 20:45:55 +0000

    Add number holes round type filter to all dashboards

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,15 +6,61 @@ var middleware = require("../middleware");
 
 router.get("/", middleware.isLoggedIn, function(req, res){
     Round.find({"player.id": req.user._id}).populate("course").exec(function(err, rounds){
+        var numFullRounds = 0,
+            numNineRounds = 0;
         if(err){
             console.log(err);
         } else {
             //Dashboard data to be returned
-            var numFullRounds = 0,
-                totalFullScore = 0,
-                numNineRounds = 0,
-                totalNineScore = 0,
-                totalFullScoreToPar = 0,
+            var roundTypes = [
+                    {
+                        numHoles: '18 Holes',
+                        found: false
+                    },
+                    {
+                        numHoles: '9 Holes',
+                        found: false
+                    }
+                ];
+            rounds.forEach(function(round){
+                if(round.isFull){
+                    numFullRounds++;
+                } else {
+                    numNineRounds++;
+                }
+            });
+            if(numFullRounds > 0){
+                roundTypes[0].found = true;
+            }
+            if(numNineRounds > 0){
+                roundTypes[1].found = true;
+            }
+            res.render("dashboard/index",{
+                user:req.user, 
+                roundTypes:roundTypes});
+        }
+    });
+});
+
+router.get("/roundsdata", function(req, res){
+    Round.find({"player.id": req.user._id, isFull: req.query.isFull}).populate("course").exec(function(err, rounds){
+        if(err){
+            console.log(err);
+        } else {
+            //Dashboard data to be returned
+            var numRounds = 0,
+                totalScore = 0,
+                totalScoreToPar = 0,
+                numScoreNames = {
+                    par: 0,
+                    bogey: 0,
+                    birdie: 0,
+                    doubleBogey: 0,
+                    eagle: 0,
+                    bogeyWorse: 0,
+                    eagleBetter: 0
+                },
+                scoreByDate = [],
                 allTeeClubs = [
                     {
                         clubName:'Driver',
@@ -61,7 +107,6 @@ router.get("/", middleware.isLoggedIn, function(req, res){
                         found:false
                     }
                 ],
-                scoreByDate = [],
                 totalScoreByHolePar = {
                     parThree: {
                         score: 0,
@@ -79,108 +124,70 @@ router.get("/", middleware.isLoggedIn, function(req, res){
             rounds.forEach(function(round){
                 var roundScore = 0,
                     roundPar = 0,
-                    roundData = []
-                if(round.isFull){
-                    numFullRounds++;
-                    roundData.push(Date.UTC(round.date));
-                    round.holes.forEach(function(hole){
-                        totalFullScore += hole.score;
-                        roundScore += hole.score;
-                        roundPar += hole.par;
-                        if(hole.par === 3) {
-                            totalScoreByHolePar.parThree.score += hole.score;
-                            totalScoreByHolePar.parThree.numHoles ++;
-                        } else if(hole.par === 4) {
-                            totalScoreByHolePar.parFour.score += hole.score;
-                            totalScoreByHolePar.parFour.numHoles ++;
-                            //Found tee club
-                            allTeeClubs.forEach(function(club){
-                                if(club.clubName === hole.teeShot.teeShotClub){
-                                    club.found = true;
-                                }
-                            });
-                        } else {
-                            totalScoreByHolePar.parFive.score += hole.score;
-                            totalScoreByHolePar.parFive.numHoles ++;
-                            //Found tee club
-                            allTeeClubs.forEach(function(club){
-                                if(club.clubName === hole.teeShot.teeShotClub){
-                                    club.found = true;
-                                }
-                            });
-                        }
-                    });
-                    roundData.push(totalFullScore);
-                    scoreByDate.push(roundData);
-                } else {
-                    numNineRounds++;
-                    round.holes.forEach(function(hole){
-                        totalNineScore += hole.score;
-                        if(hole.par === 3) {
-                            totalScoreByHolePar.parThree.score += hole.score;
-                            totalScoreByHolePar.parThree.numHoles ++;
-                        } else if(hole.par === 4) {
-                            totalScoreByHolePar.parFour.score += hole.score;
-                            totalScoreByHolePar.parFour.numHoles ++;
-                            //Found tee club
-                            allTeeClubs.forEach(function(club){
-                                if(club.clubName === hole.teeShot.teeShotClub){
-                                    club.found = true;
-                                }
-                            });
-                        } else {
-                            totalScoreByHolePar.parFive.score += hole.score;
-                            totalScoreByHolePar.parFive.numHoles ++;
-                            //Found tee club
-                            allTeeClubs.forEach(function(club){
-                                if(club.clubName === hole.teeShot.teeShotClub){
-                                    club.found = true;
-                                }
-                            });
-                        }
-                    });
-                }
-                totalFullScoreToPar += (roundScore - roundPar);
-                
-            });
-            var ScoreData =
-                {
-                    avgNineScore: totalNineScore / numNineRounds,
-                    avgFullScore: totalFullScore / numFullRounds,
-                    avgFullScoreToPar: totalFullScoreToPar / numFullRounds,
-                    scoreByDate: scoreByDate,
-                    avgScoreByHolePar: {
-                        parThree: totalScoreByHolePar.parThree.score / totalScoreByHolePar.parThree.numHoles,
-                        parFour: totalScoreByHolePar.parFour.score / totalScoreByHolePar.parFour.numHoles,
-                        parFive: totalScoreByHolePar.parFive.score / totalScoreByHolePar.parFive.numHoles
+                    roundData = [];
+                numRounds++;
+                roundData.push(round.date);
+                round.holes.forEach(function(hole){
+                    totalScore += hole.score;
+                    roundScore += hole.score;
+                    roundPar += hole.par;
+                    //Set Score Names
+                    if(hole.score - hole.par === 0){
+                        numScoreNames.par++;
+                    } else if(hole.score - hole.par === 1){
+                        numScoreNames.bogey++;
+                    } else if(hole.score - hole.par === -1){
+                        numScoreNames.birdie++;
+                    } else if(hole.score - hole.par === 2){
+                        numScoreNames.doubleBogey++;
+                    } else if(hole.score - hole.par === -2){
+                        numScoreNames.eagle++;
+                    } else if(hole.score - hole.par > 2){
+                        numScoreNames.bogeyWorse++;
+                    } else if(hole.score - hole.par < -2){
+                        numScoreNames.eagleBetter++;
                     }
-                };
-            res.render("dashboard/index",{user:req.user, rounds:rounds, ScoreData, allTeeClubs:allTeeClubs});
+                    if(hole.par === 3) {
+                        totalScoreByHolePar.parThree.score += hole.score;
+                        totalScoreByHolePar.parThree.numHoles ++;
+                    } else if(hole.par === 4) {
+                        totalScoreByHolePar.parFour.score += hole.score;
+                        totalScoreByHolePar.parFour.numHoles ++;
+                        //Found tee club
+                        allTeeClubs.forEach(function(club){
+                            if(club.clubName === hole.teeShot.teeShotClub){
+                                club.found = true;
+                            }
+                        });
+                    } else {
+                        totalScoreByHolePar.parFive.score += hole.score;
+                        totalScoreByHolePar.parFive.numHoles ++;
+                        //Found tee club
+                        allTeeClubs.forEach(function(club){
+                            if(club.clubName === hole.teeShot.teeShotClub){
+                                club.found = true;
+                            }
+                        });
+                    }
+                });
+                roundData.push(roundScore);
+                scoreByDate.push(roundData);
+                totalScoreToPar += (roundScore - roundPar);
+            }); 
         }
+        var avgScore =
+            {
+                avgScore: totalScore / numRounds,
+                avgScoreToPar: totalScoreToPar / numRounds,
+                scoreByDate: scoreByDate,
+                avgScoreByHolePar: {
+                    parThree: totalScoreByHolePar.parThree.score / totalScoreByHolePar.parThree.numHoles,
+                    parFour: totalScoreByHolePar.parFour.score / totalScoreByHolePar.parFour.numHoles,
+                    parFive: totalScoreByHolePar.parFive.score / totalScoreByHolePar.parFive.numHoles
+                }
+            };
+        res.send({rounds:rounds, avgScore, user: req.user, numScoreNames, allTeeClubs:allTeeClubs});
     });
 });
-
-// //ajax
-// router.get("/teeclub", middleware.isLoggedIn, function(req, res){
-//     var teeClubSelect = req.query.teeClub;
-//     if(teeClubSelect === "All"){
-//         Round.findOne({name: teeClubSelect}).exec(function(err, course){
-//             if(err){
-//                 console.log(err);
-//             } else {
-//                 res.send({course:course}); 
-//             }
-//         });
-//     } else {
-        
-//     }
-//     Course.findOne({name: courseSelect}).exec(function(err, course){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             res.send({course:course}); 
-//         }
-//     });
-// });
 
 module.exports = router;
