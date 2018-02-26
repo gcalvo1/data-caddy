d4433569bd125e8d477f723733eccf3e8fc97d64
George Calvo <george.r.calvo@gmail.com>
2018-02-26 15:47:51 +0000

    Refactor round index view

--- public/scripts/rounds/index.js
+++ public/scripts/rounds/index.js
@@ -1,3 +1,50 @@
 //Set Active Menu
 $('.menu_options li').removeClass('active');
 $('#rounds_menu_option').addClass('active');
+
+$('#pagination-demo').twbsPagination({
+    totalPages: $('#hidden_page').text(),
+    // the current page that show on start
+    startPage: 1,
+    
+    // maximum visible pages
+    visiblePages: 5,
+    
+    initiateStartPageClick: true,
+    
+    // template for pagination links
+    href: false,
+    
+    // variable name in href template for page number
+    hrefVariable: '{{number}}',
+    
+    // Text labels
+    first: 'First',
+    prev: 'Previous',
+    next: 'Next',
+    last: 'Last',
+    
+    // carousel-style pagination
+    loop: false,
+    
+    // callback function
+    onPageClick: function (event, page) {
+    $('.page-active').removeClass('page-active');
+    $('#page'+page).addClass('page-active');
+    },
+    
+    // pagination Classes
+    paginationClass: 'pagination',
+    nextClass: 'next',
+    prevClass: 'prev',
+    lastClass: 'last',
+    firstClass: 'first',
+    pageClass: 'page',
+    activeClass: 'active',
+    disabledClass: 'disabled'
+
+});
+    
+if(parseInt($('#hidden_page').text()) === 1) {
+    $('#pagination-demo').addClass("hidden");
+}
