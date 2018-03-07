5e156f0b063d00254f05c5f034bcff79f6b30f78
George Calvo <george.r.calvo@gmail.com>
2018-03-07 19:20:18 +0000

    Fade out flash messages

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
     $(".dropdown-toggle").dropdown();
 });
+
+$('.alert').fadeOut(10000);
--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,3 +1,5 @@
 //Set Active Menu
 $('.menu_options li').removeClass('active');
 $('#home_menu_option').addClass('active');
+
+$('.alert').fadeOut(10000);
