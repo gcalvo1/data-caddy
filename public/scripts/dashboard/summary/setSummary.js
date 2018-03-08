6dbe5c3c10dbe851d7bbf30bd4cc0b8873f5f589
George Calvo <george.r.calvo@gmail.com>
2018-03-08 18:45:08 +0000

    Add sign to avg score to par

--- public/scripts/dashboard/summary/setSummary.js
+++ public/scripts/dashboard/summary/setSummary.js
@@ -12,7 +12,13 @@ function setSummary(parameters){
         //Set Summary Data Cards
         $('#scoring-avg').html(Math.round(data.avgScore.avgScore * 10) / 10);
         $('#scoring-avg').attr("data-to",Math.round(data.avgScore.avgScore * 10) / 10);
-        $('#avg-score-to-par').html(Math.round(data.avgScore.avgScoreToPar * 10) / 10);
+        var sign = "+";
+        if(data.avgScore.avgScoreToPar === 0) {
+            sign = "";
+        } else if(data.avgScore.avgScoreToPar < 0) {
+            sign = "-";
+        }
+        $('#avg-score-to-par').html(sign + String(Math.round(data.avgScore.avgScoreToPar * 10) / 10));
         $('#avg-score-to-par').attr("data-to",Math.round(data.avgScore.avgScoreToPar * 10) / 10);
         $('#par-three-scoring-avg').html(Math.round(data.avgScore.avgScoreByHolePar.parThree * 10) / 10);
         $('#par-three-scoring-avg').attr("data-to",Math.round(data.avgScore.avgScoreByHolePar.parThree * 10) / 10);
