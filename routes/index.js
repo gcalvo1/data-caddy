gcalvo1/data-caddy

commit 8d70453d759b0ad32a285703189545e366953f30

Add profile picture to header

routes/index.js
@@ -12,12 +12,18 @@ var sgMail = require('@sendgrid/mail');
 var randomstring = require("randomstring");
 var middleware = require("../middleware");
 
-// AWS.config.loadFromPath('./s3_config.json');
-var s3 = new AWS.S3();
+var s3Bucket = new AWS.S3({ params: {Bucket: 'data-caddy-profile-pics'} });
 
 //Route route
 router.get("/", function(req,res){
-    res.render("landing");
+    if(req.user){
+        var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
+        s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
+            res.render("landing",{userImg: url});
+        });
+      } else {
+          res.render("landing",{userImg: null});  
+      }
 });
 
 //==================Auth Routes==========================
@@ -435,8 +441,6 @@ router.post('/reset/:token', function(req, res) {
 
 //User Profile Route
 router.get("/profile", middleware.isLoggedIn, function(req, res){
-    // AWS.config.loadFromPath('./s3_config.json');
-            
     var s3Bucket = new AWS.S3({ params: {Bucket: 'data-caddy-profile-pics'} });
     var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
     s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){

app.js
@@ -9,7 +9,6 @@ var express = require("express"),
     Course = require("./models/course"),
     Round = require("./models/round"),
     User = require("./models/user"),
-    AWS = require('aws-sdk'),
     seedDB = require("./seeds");
     
 require('dotenv').config();
@@ -25,7 +24,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public")); 
 app.use(methodOverride("_method"));
 app.use(flash());
-seedDB();
+//seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({

public/stylesheets/main.css
@@ -289,6 +289,18 @@ body {
 	margin-bottom: 20px;
 }
 
+#header-user-info {
+    padding-top: 13px;
+    padding-bottom: 13px;
+}
+
+#header-prof-pic {
+    width: 25px;
+    height: 25px;
+    margin-right: 3px;
+    border-radius: 50%;
+}
+
 hr {
 	 border-color: #DDCECE;
 	 background-color: #DDCECE;

routes/rounds.js
@@ -5,14 +5,19 @@ var Course = require("../models/course");
 var User = require("../models/user");
 var middleware = require("../middleware");
 var request = require('request');
+var AWS = require('aws-sdk');
+
+var s3Bucket = new AWS.S3({ params: {Bucket: 'data-caddy-profile-pics'} });
 
 router.get("/", middleware.isLoggedIn, middleware.emailVerified, function(req, res){
     Round.find({"player.id": req.user._id}).populate("course").exec(function(err, rounds){
         if(err){
             console.log(err);
         } else {
-            //console.log({rounds:rounds});
-            res.render("rounds/index",{rounds:rounds});
+            var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
+            s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
+                res.render("rounds/index",{rounds:rounds,userImg: url});
+            });
         }
     });
 });
@@ -208,7 +213,10 @@ router.get("/new", middleware.isLoggedIn, middleware.emailVerified, function(req
         if(err){
             console.log(err);
         } else {
-            res.render("rounds/new",{courses:courses, user: req.user}); 
+            var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
+            s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
+                res.render("rounds/new",{courses:courses, user: req.user, userImg: url}); 
+            });
         }
     });
 });
@@ -220,8 +228,10 @@ router.get("/:id", middleware.isLoggedIn, middleware.emailVerified, function(req
       if(err){
           console.log(err);
       } else {
-          console.log(req.user);
-          res.render("rounds/show", {round: foundRound, user: req.user});
+          var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
+          s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
+            res.render("rounds/show", {round: foundRound, user: req.user, userImg: url});
+          });
       }
     });
 });
@@ -240,7 +250,10 @@ router.get("/:id/edit", middleware.checkRoundOwnership, function(req, res) {
                         if(err){
                             console.log(err);
                         } else {
-                            res.render("rounds/edit",{round: foundRound, courses:courses, course:course, user:req.user}); 
+                            var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
+                            s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
+                                res.render("rounds/edit",{round: foundRound, courses:courses, course:course, user:req.user, userImg: url}); 
+                            });
                         }
                     });
                 }

views/partials/header.ejs
@@ -14,7 +14,6 @@
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
         <script src=https://cdnjs.cloudflare.com/ajax/libs/datepicker/0.6.4/datepicker.js></script>
-        <!--<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript" async></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
     </head>
@@ -68,11 +67,11 @@
                             </form>
                         <% } else { %>
                             <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href=""><% if(!currentUser.hasImg || currentUser.hasImg) { %>
+                                <a id="header-user-info" class="dropdown-toggle" data-toggle="dropdown" href=""><% if(!currentUser.hasImg || !userImg) { %>
                         <span class="glyphicon glyphicon-user"></span>
-                    <% } %> <!--else { %>-->
-                    <!--    <span><img id='dash-prof-pic' src='<= currentUser.imgLink %>'></span>-->
-                    <!--< } %> -->
+                    <% } else { %>
+                            <span><img id='header-prof-pic' src='<%= userImg %>'></span>
+                    <% } %>
                         </span>      <%= currentUser.name %> </span>
                                 <span class="caret"></span></a>
                                 <ul class="dropdown-menu">

views/partials/landing_header.ejs
@@ -44,11 +44,11 @@
                             </form>
                         <% } else { %>
                             <li class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href=""><% if(!currentUser.hasImg || currentUser.hasImg) { %>
+                                <a class="dropdown-toggle" data-toggle="dropdown" href=""><% if(!currentUser.hasImg || !userImg) { %>
                         <span class="glyphicon glyphicon-user"></span>
-                    <% } %> <!--else { %>-->
-                    <!--    <span><img id='dash-prof-pic' src='<= currentUser.imgLink %>'></span>-->
-                    <!--< } %> -->
+                    <% } else { %>
+                            <span><img id='header-prof-pic' src='<%= userImg %>'></span>
+                    <% } %>
                         </span>      <%= currentUser.name %> </span>
                                 <span class="caret"></span></a>
                                 <ul class="dropdown-menu">

commit 9d75809f1ceff1f344b1e0c8828ed0f698128618

Fix s3 variable naming bug broken in commit 8d70453d759b0ad32a285703189545e366953f30

routes/index.js
@@ -37,7 +37,7 @@ router.post("/register", function(req, res){
         imgExt = "";
     const upload = multer({
         storage: multerS3({
-            s3: s3,
+            s3: s3Bucket,
             bucket: 'data-caddy-profile-pics',
             acl: 'public-read',
             key: function (request, file, cb) {