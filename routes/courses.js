90cb30805297b3cf2e0230f2b031bfdf90aedabf
George Calvo <george.r.calvo@gmail.com>
2018-12-28 16:30:50 +0000

    Course slope/rating float bug fix

--- routes/courses.js
+++ routes/courses.js
@@ -124,7 +124,6 @@ router.get("/new", middleware.isLoggedIn, middleware.emailVerified, function(req
 
 //SHOW
 router.get("/:id", middleware.isLoggedIn, middleware.emailVerified, function(req, res) {
-    //find the round with id and render show page with the round
     Course.findById(req.params.id).exec(function(err, foundCourse){
       if(err){
           console.log(err);
@@ -145,9 +144,97 @@ router.get("/:id/edit", middleware.isLoggedIn, middleware.isAdmin, function(req,
         } else {
                     var urlParams = {Bucket: 'data-caddy-profile-pics', Key: req.user.username + req.user.imgExt};
                     s3Bucket.getSignedUrl('getObject', urlParams, function(err, url){
-                        res.render("courses/edit",{round: foundCourse, user:req.user, userImg: url}); 
+                        res.render("courses/edit",{course: foundCourse, user:req.user, userImg: url}); 
                     });
                 }
+    });
+});
+
+//UPDATE
+router.put("/:id", function(req, res){
+    var isNine = false,
+    isNineOnly = false;
+    if(req.body.course.isNine == ""){
+        isNine = true;
+    }
+    if(req.body.course.isNineOnly == ""){
+        isNineOnly = true;
+    }
+    
+    //Create tees array
+    var found = true,
+        count = 1,
+        tees = [];
+    while(found){
+        if(eval("req.body.course.color_" + count)){
+            tees.push({
+                color: eval("req.body.course.color_" + count),
+                rating: eval("req.body.course.rating_" + count),
+                slope: eval("req.body.course.slope_" + count)
+            });
+            count++;
+        } else {
+            found = false;
+        }        
+    }
+    
+    //Create holes array
+    var num_holes = 18,
+        holes = [];
+    if(isNineOnly){
+        num_holes = 9;
+    }
+    for(var i=1;i<=num_holes;i++){
+        var teeFound = true,
+            teeCount = 1,
+            yardage = {};
+        while(teeFound){
+            console.log(eval("req.body.course.tee_label_" + teeCount));
+            console.log(eval("req.body.course.yardage_" + teeCount + "[" + i + "-1]"));
+            if(eval("req.body.course.tee_label_" + teeCount)){
+                yardage[eval("req.body.course.tee_label_" + teeCount)] = eval("req.body.course.yardage_" + teeCount + "[" + i + "-1]");
+                teeCount++;
+            } else {
+                teeFound = false;
+            }        
+        }
+        
+        holes.push({
+            number: i,
+            par: eval("req.body.course.par_" + i),
+            handicap: eval("req.body.course.handicap_" + i),
+            yardage: yardage
+        });
+        
+        console.log(yardage);
+    }
+    
+    var img = "../img/" + req.body.course.name.split(' ').join('_') + "-Logo.png";
+    
+    var updateObj = {
+        name: req.body.course.name,
+        img: img,
+        location: {
+          country: req.body.course.country,
+          state: req.body.course.state,
+          city: req.body.course.city,  
+          zip: req.body.course.zip,
+          latitude: req.body.course.latitude,
+          longitude: req.body.course.longitude
+        },
+        isNine: isNine,
+        isNineOnly: isNineOnly,
+        tees: tees,
+        holes: holes,
+        loadDate: Date.now()
+    };
+    
+    Course.findByIdAndUpdate(req.body.id, updateObj, function(err, updatedCourse){
+        if(err){
+            res.redirect("/courses");
+        } else {
+            res.redirect("/courses");
+        }
     });
 });
 
