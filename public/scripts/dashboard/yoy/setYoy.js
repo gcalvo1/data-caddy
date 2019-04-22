2cbf02d33823eb98d7eb9804bd0b5849608d7649
George Calvo <george.r.calvo@gmail.com>
2019-04-22 18:16:21 +0000

    Add YoY total score

--- public/scripts/dashboard/yoy/setYoy.js
+++ public/scripts/dashboard/yoy/setYoy.js
@@ -25,26 +25,29 @@ function setYoy(parameters) {
             numRoundsCompare = numRoundsPrevYear;
         }
         
-        var currentYearTotalScoreToPar = 0;
-        var prevYearTotalScoreToPar = 0;
+        var currentYearTotalScoreToPar = 0,
+            prevYearTotalScoreToPar = 0,
+            currentYearTotalScore = 0,
+            prevYearTotalScore = 0;
         for(var i=0;i<numRoundsCompare;i++){
             currentYearRounds[i].holes.forEach(function(hole){
+                currentYearTotalScore = currentYearTotalScore + hole.score;
                 currentYearTotalScoreToPar = currentYearTotalScoreToPar + (hole.score - hole.par);
             });
             prevYearRounds[i].holes.forEach(function(hole){
+                prevYearTotalScore = prevYearTotalScore + hole.score;
                 prevYearTotalScoreToPar = prevYearTotalScoreToPar + (hole.score - hole.par);
             });
         }
         
-        var currYearAvgScoreToPar = currentYearTotalScoreToPar / numRoundsCompare;
-        var prevYearAvgScoreToPar = prevYearTotalScoreToPar / numRoundsCompare;
-        console.log(currYearAvgScoreToPar);
-        console.log(prevYearAvgScoreToPar);
-        console.log(currentYearRounds);
-        console.log(prevYearRounds);
-        
+        var currYearAvgScoreToPar = currentYearTotalScoreToPar / numRoundsCompare,
+            prevYearAvgScoreToPar = prevYearTotalScoreToPar / numRoundsCompare,
+            currYearAvgScore = currentYearTotalScore / numRoundsCompare,
+            prevYearAvgScore = prevYearTotalScore / numRoundsCompare;
+            
         $('#yoy-title').html("Year-Over-Year Data Through " + numRoundsCompare + " Rounds");
         $('#yoy-years').html("Current Year: " + currentYear + " | Previous Year: " + prevYear);
+        
         $('#current-year-strokes-to-par').html(Math.round(currYearAvgScoreToPar * 10) / 10);
         $('#prev-year-strokes-to-par').html(Math.round(prevYearAvgScoreToPar * 10) / 10);
         $('#current-year-strokes-to-par-p').html(currentYear + " Strokes to Par");
@@ -54,6 +57,17 @@ function setYoy(parameters) {
             $('#strokes-to-par-change').addClass('pos-change');
         } else if ( currYearAvgScoreToPar - prevYearAvgScoreToPar > 0 ) {
             $('#strokes-to-par-change').addClass('neg-change');
+        }
+        
+        $('#current-year-score').html(Math.round(currYearAvgScore * 10) / 10);
+        $('#prev-year-score').html(Math.round(prevYearAvgScore * 10) / 10);
+        $('#current-year-score-p').html(currentYear + " Score");
+        $('#prev-year-score-p').html(prevYear + " Score");
+        $('#score-change').html(Math.abs(Math.round((((currYearAvgScore - prevYearAvgScore) / prevYearAvgScore) * 100) * 10) / 10) + "%");
+        if( currYearAvgScore - prevYearAvgScore < 0 ) {
+            $('#score-change').addClass('pos-change');
+        } else if ( currYearAvgScore - prevYearAvgScore > 0 ) {
+            $('#score-change').addClass('neg-change');
         }
     });
 }
