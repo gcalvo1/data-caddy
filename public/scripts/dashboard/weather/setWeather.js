d73b80b5ac7bd0214b80b6aa6b63cebc15a348df
George Calvo <George.Calvo@mlb.com>
2020-01-02 16:23:00 -0500

    Remove extra '-' sign from weather scores

--- public/scripts/dashboard/weather/setWeather.js
+++ public/scripts/dashboard/weather/setWeather.js
@@ -3,13 +3,7 @@ function setWeather(parameters) {
         $('.card').addClass('hidden');
         data.weatherIcons.forEach(function(icon){
             $('#'+icon.icon).removeClass('hidden');
-            var sign = "+";
-            if(icon.scoreToPar == 0){
-                sign = "";
-            } else if(icon.scoreToPar < 0) {
-                sign = "-"
-            }
-            $('#'+icon.icon+ ' .card-temp').html(sign + String(Math.round((icon.scoreToPar / icon.rounds) * 10) / 10));
+            $('#'+icon.icon+ ' .card-temp').html(String(Math.round((icon.scoreToPar / icon.rounds) * 10) / 10));
         });
         var windHtml = "",
             numWindCategories = data.windSpeed.length,
@@ -23,13 +17,7 @@ function setWeather(parameters) {
         }
         data.windSpeed.sort(compare);
         data.windSpeed.forEach(function(category){
-            var sign = "+";
-            if(category.scoreToPar == 0){
-                sign = "";
-            } else if(category.scoreToPar < 0) {
-                sign = "-"
-            }
-            var scoreToParAvg = sign + String(Math.round((category.scoreToPar / category.rounds) * 10) / 10);
+            var scoreToParAvg = String(Math.round((category.scoreToPar / category.rounds) * 10) / 10);
             var firPercent = String(Math.round(((category.roundFirs / category.roundTeeShots) * 100 ) * 10) / 10) + "%";
             var girPercent = String(Math.round(((category.roundGirs / category.roundApproaches) * 100 ) * 10) / 10) + "%";
             windHtml += '<div class="col-md-'+colLen+'"><div class="counter"><div class="row"><div><div class="col-md-4 no-col-pad"><h2 class="timer count-title count-number" data-to="" data-speed="1500">'+scoreToParAvg+'</h2><p class="count-text ">Score to Par</p></div><div class="col-md-4 no-col-pad"><h2 class="timer count-title count-number" data-to="" data-speed="1500">'+firPercent+'</h2><p class="count-text">FIR %</p></div><div class="col-md-4 no-col-pad"><h2 class="timer count-title count-number" data-to="" data-speed="1500">'+girPercent+'</h2><p class="count-text ">GIR %</p></div></div></div><div class="row"><p class="count-text wind-category-label">'+category.category+' Wind</p></div></div></div></div>';
@@ -53,13 +41,7 @@ function setWeather(parameters) {
         }
         data.temperature.sort(compare);
         data.temperature.forEach(function(category){
-            var sign = "+";
-            if(category.scoreToPar == 0){
-                sign = "";
-            } else if(category.scoreToPar < 0) {
-                sign = "-"
-            }
-            var scoreToParAvg = sign + String(Math.round((category.scoreToPar / category.rounds) * 10) / 10);
+            var scoreToParAvg = String(Math.round((category.scoreToPar / category.rounds) * 10) / 10);
             var firPercent = String(Math.round(((category.roundFirs / category.roundTeeShots) * 100 ) * 10) / 10) + "%";
             var girPercent = String(Math.round(((category.roundGirs / category.roundApproaches) * 100 ) * 10) / 10) + "%";
             tempHtml += '<div class="col-md-'+colLen+'"><div class="counter"><div class="row"><div><div class="col-md-4 no-col-pad"><h2 class="timer count-title count-number" data-to="" data-speed="1500">'+scoreToParAvg+'</h2><p class="count-text ">Score to Par</p></div><div class="col-md-4 no-col-pad"><h2 class="timer count-title count-number" data-to="" data-speed="1500">'+firPercent+'</h2><p class="count-text">FIR %</p></div><div class="col-md-4 no-col-pad"><h2 class="timer count-title count-number" data-to="" data-speed="1500">'+girPercent+'</h2><p class="count-text ">GIR %</p></div></div></div><div class="row"><p class="count-text wind-category-label">'+category.category+'</p></div></div></div></div>';
